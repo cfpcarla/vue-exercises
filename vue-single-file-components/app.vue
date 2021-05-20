5c49e0501fb8496e69097f5cf3e8f48e9fea6e5a
Carla <cfpcarla@gmail.com>
2021-05-20 10:14:40 -0400

    Add exercise hello world and exercise data-binding

---
+++ vue-single-file-components/app.vue
@@ -0,0 +1,23 @@
+// Example data-binding
+<template>
+  <div> 
+  <h1 :style="{ textDecoration: decoration }">Hello {{ name }}</h1>
+  <input type="text" v-model="name">
+  <br>
+  <a :href="link"> Link para o curso!</a>
+  </div>
+</template>
+
+<script>
+export default {
+data: () => ({
+    name:'Carla',
+    link:'https://mustache.github.io/mustache.5.html',
+    decoration:'underline'
+})
+}
+</script>
+
+<style>
+
+</style>
